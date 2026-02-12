713ace37ab6fe8f8b9b0a2eb41f71b39ac7c6a64
aramimn <araminoguera08@gmail.com>
2026-02-12 12:12:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,4 @@ const secondScreen = document.getElementById("second-screen");
 yesBtn.addEventListener("click", () => {
     firstScreen.style.display = "none";
     secondScreen.style.display = "flex";
-    secondScreen.style.opacity = "0";
-
-    setTimeout(() => {
-        secondScreen.style.transition = "opacity 0.8s ease";
-        secondScreen.style.opacity = "1";
-    }, 50);
 });
